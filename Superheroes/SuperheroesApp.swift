a05ffbca60e3728a98359b723138e26b6cda3e77
27ugeen <27ugeen@gmail.com>
2022-05-16 18:46:38 +0300

    Initial Commit

---
+++ Superheroes/SuperheroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperheroesApp.swift
+//  Superheroes
+//
+//  Created by GiN Eugene on 16/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperheroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
